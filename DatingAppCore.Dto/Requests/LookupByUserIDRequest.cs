52dda0e4042908ebaaee41e5fe3195870a6df4bc
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2019-06-10 21:35:11 -0400

    Set default skip and take

--- DatingAppCore.Dto/Requests/LookupByUserIDRequest.cs
+++ DatingAppCore.Dto/Requests/LookupByUserIDRequest.cs
@@ -7,8 +7,8 @@ namespace DatingAppCore.Dto.Requests
     public class LookupByUserIDRequest
     {
         public Guid UserID { get; set; }
-        public int Skip { get; set; }
-        public int Take { get; set; }
+        public int Skip { get; set; } = 0;
+        public int Take { get; set; } = 1000;
         public DateTime From { get; set; }
         public DateTime To { get; set; }
     }
