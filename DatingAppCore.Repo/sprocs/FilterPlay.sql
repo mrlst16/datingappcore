ea1f4459e05c2fee88fd5a9d13ccc6428b4b2ea3
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2019-07-08 23:06:22 -0400

    Save

--- DatingAppCore.Repo/sprocs/FilterPlay.sql
+++ DatingAppCore.Repo/sprocs/FilterPlay.sql
@@ -1,13 +1,14 @@
 Begin Tran
 
 declare @userid uniqueidentifier = 'B720EFB5-6DBC-4F5D-8AA8-D1516158B207'
-declare @kvpString nvarchar(max) = 'sex=m, gender = m';
+declare @kvpString nvarchar(max) = 'sex=f, gender = f,   dogs=t';
 
 exec SearchUsers @userid, 0, 1000, @kvpString
 
 select *
 from UserProfileField
-where Name in ('sex', 'gender')
+where Name in ('sex', 'gender', 'dogs')
+and IsSetting = 0
 order by UserID, Name
 
 Rollback Tran
