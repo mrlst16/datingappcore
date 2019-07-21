e971bc9fb632f206f311e24d54b8caa6979a3a59
LAPTOP-R0OK2Q7S\matt <mrlst16@mail.rmu.edu>
2019-07-21 16:04:34 -0400

    hooked up the signalr experiment

--- DatingAppCore.Api/ChatHub.cs
+++ DatingAppCore.Api/ChatHub.cs
@@ -10,54 +10,21 @@ using System.Threading.Tasks;
 
 namespace DatingAppCore.Api
 {
-
-    //public class Conversation
-    //{
-    //    public Guid ID { get; set; }
-    //    public string ConnectionID { get; set; }
-    //    public Guid User1ID { get; set; }
-    //    public Guid User2ID { get; set; }
-    //}
-
     public class ChatHub : Hub
     {
-        protected class ConversationComparer : IEqualityComparer<ConversationDTO>
-        {
-            public bool Equals(ConversationDTO one, ConversationDTO two)
-            {
-                return
-                    (
-                        one.ID == two.ID
-                        && one.ID != Guid.Empty
-                        && two.ID != Guid.Empty
-                    )
-                    || (one.User1ID == two.User1ID && one.User2ID == two.User2ID)
-                    || (one.User1ID == two.User2ID && one.User2ID == two.User1ID);
-            }
-
-            public int GetHashCode(ConversationDTO obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
-
-        private List<ConversationDTO> conversations
-            = new List<ConversationDTO>();
-
         public ChatHub()
         {
         }
 
-        public async Task SendMessageToGroup(string user, string message)
+        public async Task SendMessage(string from, string to, string message)
         {
             try
             {
-                var conversation = conversations.Find(x => x.ConnectionID == Context.ConnectionId);
-                await Clients.Group(conversation.ID.ToString()).SendAsync($"ReceiveMessage", user, message);
+                var conversation = await LoginOrRegisterConversation(from, to);
+                await Clients.Group(conversation.ID.ToString()).SendAsync($"ReceiveMessage", from, to, message);
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -92,20 +59,15 @@ namespace DatingAppCore.Api
         private async Task<ConversationDTO> LoginOrRegisterConversation(string from, string to)
         {
             ConversationDTO conversation = null;
-            if (Guid.TryParse(from, out Guid fromID) && Guid.TryParse(from, out Guid toID))
+            if (Guid.TryParse(from, out Guid fromID) && Guid.TryParse(to, out Guid toID))
             {
-                conversation = conversations.FirstOrDefault(
-                    (x) => (x.User1ID == fromID && x.User2ID == toID)
-                    || (x.User1ID == toID && x.User2ID == fromID)
-                );
-                if (conversation != null) return conversation;
-
                 var lookupConversationService = new LookupConversationService();
-                Response<ConversationDTO> response = await lookupConversationService.Lookup(new Dto.Requests.GetConversationRequest()
-                {
-                    User1ID = fromID,
-                    User2ID = toID
-                });
+                Response<ConversationDTO> response = await lookupConversationService
+                    .Lookup(new Dto.Requests.GetConversationRequest()
+                    {
+                        User1ID = fromID,
+                        User2ID = toID
+                    });
 
                 if (response.Sucess)
                 {
